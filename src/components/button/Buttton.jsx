b44bc6d37cda94c5e533ee4090ff341a394d851a
geogrey12muiruri <greggambugua@gmail.com>
2024-01-19 12:31:25 -0800

    added some few modifications

--- src/components/button/Buttton.jsx
+++ src/components/button/Buttton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Buttton = () => {
   return (
     <div>
-      <button className='bg-pink-500  p-4 bg-blend-saturation px-5 font-serif hover:bg-red-50  border-spacing-1 text-black font-bold py-2  rounded mr-6'>Contact Us</button>
+      <button className='bg-red-50  p-4 bg-blend-saturation px-5 font-serif hover:bg-pink-500  border-spacing-1 text-black font-bold py-2  rounded mr-6'>Contact Us</button>
     </div>
   )
 }
--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
         <p className="text-lg mb-4 font-serif">from 105$</p>
         <div className="flex items-center indent-6 ">
         <Link to="/design" className="text-gray-900 hover:text-orange-950">
-        <button className="bg-white bg-blend-saturation px-5 font-serif hover:bg-red-50  border-spacing-1 text-black font-bold py-2  rounded mr-6">
+        <button className="bg-red-50 bg-blend-saturation px-5 font-serif hover:bg-pink-500  border-spacing-1 text-black font-bold py-2  rounded mr-6">
             Design
           </button>
                 
