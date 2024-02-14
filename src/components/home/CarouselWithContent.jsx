5e01d3281f7118a8bdc32395c1e3f21253a693f0
geogrey12muiruri <greggambugua@gmail.com>
2024-02-13 23:47:00 -0800

    added responsivenes to the carousel component

--- src/components/home/CarouselWithContent.jsx
+++ src/components/home/CarouselWithContent.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Carousel } from "@material-tailwind/react";
 import "./carousel.css";
 
-
 export function CarouselWithContent() {
   const [carouselImages, setCarouselImages] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -32,7 +31,7 @@ export function CarouselWithContent() {
   };
 
   return (
-    <div className="carousel-container relative h-96 overflow-hidden rounded-lg">
+    <div className="carousel-container relative h-96 sm:h-auto overflow-hidden rounded-lg">
       <Carousel
         items={carouselImages} // Use updated carouselImages state
         currentIndex={currentIndex}
@@ -56,18 +55,18 @@ export function CarouselWithContent() {
       {/* Overlay content with conditional rendering */}
       {showWelcomeText ? (
         <div className="overlay absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center text-white">
-          <p className="text-4xl font-bold mb-4 text-slate-950 mix-blend-hard-light">
+          <p className="text-4xl sm:text-2xl font-bold mb-4 text-slate-950 mix-blend-hard-light">
             Tailor-Made Lets You Design
           </p>
-          <p className="text-4xl font-bold mb-4 text-slate-950 mix-blend-hard-light">
+          <p className="text-4xl sm:text-2xl font-bold mb-4 text-slate-950 mix-blend-hard-light">
             Bring Your Imaginations to Life
           </p>
         </div>
       ) : (
         <div className="absolute top-1/2 left-0 right-0 p-8 transform -translate-y-1/2 flex flex-col items-center">
-              <h1 className="text-5xl font-bold font-mono text-neutral-700 leading-tight mb-4"><span className="font-bold text-pink-950 text-animate-pulse">Say goodbye </span>to off-the-rack blues. Hello, bespoke bliss.</h1>
-              <p className="text-lg font-mono text-pink-950 text-center mb-8">Unleash your inner fashion icon. We will find the perfect stitch for your story.</p>
-              <a href="#" className="bg-pink-500 text-slate-950 hover:bg-pink-950 text-center py-2 px-6 rounded-full text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg">Design</a>
+              <h1 className="text-5xl sm:text-3xl font-bold font-mono text-neutral-700 leading-tight mb-4"><span className="font-bold text-pink-950 text-animate-pulse">Say goodbye </span>to off-the-rack blues. Hello, bespoke bliss.</h1>
+              <p className="text-lg sm:text-base font-mono text-pink-950 text-center mb-8">Unleash your inner fashion icon. We will find the perfect stitch for your story.</p>
+              <a href="#" className="bg-pink-500 text-slate-950 hover:bg-pink-950 text-center py-2 px-6 rounded-full text-lg sm:text-base font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg">Design</a>
             </div>
       )}
     </div>
